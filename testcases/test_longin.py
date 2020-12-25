3a4449c83512acca91414bb46a57e6ed08456922
zhangjingwen <zjw19888148628@163.com>
2020-12-25 09:28:54 +0800

    login

---
+++ testcases/test_longin.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @TIME    :  2020/12/25
+# @Author  :  zhangjingwen
+# @File    :  test_longin
+import pytest
+
+
+class TestLogin:
+    def login(self):
+        pass
+
+
+if __name__ == '__main__':
+    pytest.main(["-s", "test_longin.py"])
